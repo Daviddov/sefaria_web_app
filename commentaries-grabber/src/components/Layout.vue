bab6730b05481ea01c4025745f0ae00686c9d3a2
dovbar <baror2009@gmail.com>
2026-06-15 14:57:27 +0200

    fix: update header title in Layout.vue for improved clarity

--- commentaries-grabber/src/components/Layout.vue
+++ commentaries-grabber/src/components/Layout.vue
@@ -61,7 +61,7 @@
     <header class="bg-white shadow-sm">
       <div class="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
         <h1 class="text-lg font-semibold leading-6 text-gray-900">
-          שליפת טקסטים מספריא
+          אגן הסהר - הזוהר על הפרשה
         </h1>
       </div>
     </header>
